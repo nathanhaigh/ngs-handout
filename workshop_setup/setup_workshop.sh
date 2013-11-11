fe67d17f52b858c97aa568e526e5df97f15958d3
paulamool <pmoolhuijzen@ccg.murdoch.edu.au>
2013-11-11 14:11:20 +0800

    Update setup_workshop.sh
    added Setup for the Software Carpentry Shell workshop lines 140-183

--- workshop_setup/setup_workshop.sh
+++ workshop_setup/setup_workshop.sh
@@ -137,6 +137,51 @@ function dl_file_from_cloud_storage() {
   fi
 }
 
+#####
+# Setup for the Software Carpentry Shell workshop
+#####
+module_dir='swc_shell'
+files=(
+  'https://swift.rc.nectar.org.au:8888/v1/AUTH_33065ff5c34a4652aa2fefb292b3195a/SWC/swc_shell.tar.gz'
+)
+echo "Setting up module: $module_dir"
+# Download the files
+echo " Downloading data files ... "
+cd "$top_dir/$data_sub_dir"
+for file in "${files[@]}"
+do
+download_file $file
+done
+
+if [[ ! -d "$top_dir/$working_dir/$module_dir" ]]; then
+mkdir -p "$top_dir/$working_dir/$module_dir"
+    cd "$top_dir/$working_dir/$module_dir"
+    tar xzf $top_dir/$data_sub_dir/swc_shell.tar.gz
+        
+    if [[ ! -e /home/$trainee_user/shell ]]; then
+su $trainee_user -c "ln -s $top_dir/$working_dir/$module_dir /home/$trainee_user/shell"
+    fi
+if [[ ! -e /home/$trainee_user/Desktop/shell ]]; then
+su $trainee_user -c "ln -s $top_dir/$working_dir/$module_dir /home/$trainee_user/Desktop/shell"
+    fi
+fi
+if [[ ! -d "/data/backup" ]]; then
+mkdir -p "/data/backup"
+    whoami > /data/access.log
+    ifconfig > /data/network.cfg
+    cat /proc/cpuinfo > /data/hardware.cfg
+fi
+if [[ ! -d "/users" ]]; then
+mkdir -p "/users"
+    ln -s /home/$trainee_user /users/vlad
+fi
+# last thing to run for this module
+if [ $(stat -c %U "$top_dir/$working_dir/$module_dir") != "$trainee_user" ]; then
+echo " Making module's working directory ($top_dir/$working_dir/$module_dir) owned by $trainee_user"
+  chown -R "$trainee_user:$trainee_user" "$top_dir/$working_dir/$module_dir"
+fi
+#########################
+
 ###############
 ## QC module ##
 ###############
