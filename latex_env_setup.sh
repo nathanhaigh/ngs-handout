dd4d4f31bf477741d6ecf3413c260ecdc30c5424
Nathan S. Watson-Haigh <nathan@watsonhaigh.net>
2013-11-20 13:48:37 +1030

    Updated Travis build files/scripts

--- latex_env_setup.sh
+++ latex_env_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # This script will install texlive and all the packages required to make the PDF handouts from the source LaTeX
 
+CTAN_MIRROR_URL='http://mirror.aarnet.edu.au/pub/CTAN'
+
 # Install texlive
-wget http://mirror.aarnet.edu.au/pub/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz
+wget ${CTAN_MIRROR_URL}/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar xzf install-tl-unx.tar.gz
-./install-tl-*/install-tl -repository http://mirror.aarnet.edu.au/pub/CTAN/systems/texlive/tlnet -no-gui -profile texlive.profile
+./install-tl-*/install-tl -repository ${CTAN_MIRROR_URL}/systems/texlive/tlnet -no-gui -profile texlive.profile
 
 # Install basic texlive scheme as we're gonna install the required packages below
-/usr/local/texlive/2013/bin/x86_64-linux/tlmgr -repository http://mirror.aarnet.edu.au/pub/CTAN/systems/texlive/tlnet install mdframed preprint enumitem etoolbox titlesec xmpincl comment latexmk lm memoir listings xcolor url l3packages l3kernel placeins microtype float latex-bin fancyhdr graphics psnfss pdftex-def oberdiek colortbl hyperref
+/usr/local/texlive/2013/bin/x86_64-linux/tlmgr -repository ${CTAN_MIRROR_URL}/systems/texlive/tlnet install mdframed preprint enumitem etoolbox titlesec xmpincl comment latexmk lm memoir listings xcolor url l3packages l3kernel placeins microtype float latex-bin fancyhdr graphics psnfss pdftex-def oberdiek colortbl hyperref
 
-# Add the latex binaries to the PATH
-export PATH=/usr/local/texlive/2013/bin/x86_64-linux:$PATH
