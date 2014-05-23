0f151ba750042f6f0fd5be1d3fb8f95aae8d988d
Nathan S. Watson-Haigh <nathan.haigh@acpfg.com.au>
2014-05-23 16:35:14 +0930

    Updated references from mm9 to mm10

--- alignment/alignment_tutorial.sh
+++ alignment/alignment_tutorial.sh
@@ -4,30 +4,30 @@
 
 cd ~/ChIP-seq
 bowtie --help
-time bowtie-build bowtie_index/mm9.fa bowtie_index/mm9               # 8m20s
+time bowtie-build bowtie_index/mm10.fa bowtie_index/mm10               # 8m20s
 ls -l bowtie_index
-time bowtie bowtie_index/mm9 -S Oct4.fastq > Oct4.sam                # 30s
+time bowtie bowtie_index/mm10 -S Oct4.fastq > Oct4.sam                # 30s
 head -n 10 Oct4.sam
 samtools view -bSo Oct4.bam Oct4.sam
 samtools flagstat Oct4.bam
 samtools sort Oct4.bam Oct4.sorted
 samtools index Oct4.sorted.bam
 genomeCoverageBed -bg -ibam Oct4.sorted.bam \
-  -g bowtie_index/mouse.mm9.genome > Oct4.bedgraph
+  -g bowtie_index/mouse.mm10.genome > Oct4.bedgraph
 bedGraphToBigWig Oct4.bedgraph \
-  bowtie_index/mouse.mm9.genome Oct4.bw
+  bowtie_index/mouse.mm10.genome Oct4.bw
 # open Oct4.sorted.bam and Oct4.bw in IGV
 
 
-time bowtie bowtie_index/mm9 -S gfp.fastq > gfp.sam                  # 30s
+time bowtie bowtie_index/mm10 -S gfp.fastq > gfp.sam                  # 30s
 head -n 10 gfp.sam
 samtools view -bSo gfp.bam gfp.sam
 samtools flagstat gfp.bam
 samtools sort gfp.bam gfp.sorted
 samtools index gfp.sorted.bam
 genomeCoverageBed -bg -ibam gfp.sorted.bam \
-  -g bowtie_index/mouse.mm9.genome > gfp.bedgraph
+  -g bowtie_index/mouse.mm10.genome > gfp.bedgraph
 bedGraphToBigWig gfp.bedgraph \
-  bowtie_index/mouse.mm9.genome gfp.bw
+  bowtie_index/mouse.mm10.genome gfp.bw
 # open gfp.sorted.bam and gfp.bw in IGV
 
